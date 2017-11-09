fcb816ab68b607b49ca11a32f43e439ede79e2e9
Valentin Ferron <ferron.vale@gmail.com>
2017-11-09 16:03:00 +0100

    finish exercice

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,49 +1,35 @@
 class DosesController < ApplicationController
-  before_action :find_dose, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @doses = Dose.all
-  end
-
-  def show
-  end
+  before_action :set_cocktail, only: [:new, :create]
 
   def new
+    @cocktail
     @dose = Dose.new
   end
 
   def create
-    @dose = Dose.new(dose_params)
+    @dose = Dose.new(set_params)
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @dose.update(dose_params)
-      redirect_to @dose
-    else
-      render :edit
-    end
-  end
-
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to doses_path
+    redirect_to dose_path
   end
 
   private
 
-  def dose_params
-    params.require(:dose).permit(:name)
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
-  def find_dose
-    @dose = Dose.find(params[:id])
+  def set_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
+
 end
